76e8d0a2ac4264639b56eda2b81d00d10f1974ff
ljaviertovar <luisjavier_tovar@outlook.com>
2024-12-24 10:36:01 -0500

    update filters

--- src/hooks/use-filters.tsx
+++ src/hooks/use-filters.tsx
@@ -56,7 +56,7 @@ export default function useFilters() {
 
 	useEffect(() => {
 		getFiltersFromUrl()
-	}, [searchParams, getFiltersFromUrl])
+	}, [searchParams])
 
 	return { filtersFromUrl, updateFilters, clearFilters }
 }
